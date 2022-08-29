5a9b74b966121eaa90b7f0c9526b5fd0fa77ca1c
jeanne desmier <desmierjeanne@gmail.com>
2022-08-29 16:37:09 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'bikes/game'
-  get 'sessions/index'
-  get 'sessions/show'
+
   devise_for :users
   root to: "pages#home"
+  resources :sessions, only: [ :index, :show ]
+  resources :bikes, only: [] do
+    get :game, on: :member
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
