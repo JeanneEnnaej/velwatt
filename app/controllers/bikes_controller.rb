48a5c3b055e72109727e2c63d1ffcdc1b564e6b1
jeanne desmier <desmierjeanne@gmail.com>
2022-08-30 15:10:30 +0200

    QR code fonctionnel localhost

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,5 +1,13 @@
 class BikesController < ApplicationController
   skip_before_action :authenticate_user!
   def game
+    # créer nouvelle session et mettre l'id en interpolation pour QR code
+    @qr_code = RQRCode::QRCode.new("http://localhost:3000/gamingsessions/2")
+    @svg = @qr_code.as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 6
+    )
   end
 end
