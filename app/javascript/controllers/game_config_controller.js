a65575e822b933d3b2f4b88c36f7954bd2acc921
amlds.dev@gmail.com <amlds.dev@gmail.com>
2022-08-31 21:03:15 +0200

    Visualisation DATA + optimisation fluidité

--- app/javascript/controllers/game_config_controller.js
+++ app/javascript/controllers/game_config_controller.js
@@ -1,11 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
+let OldProduct = 0;
+let Compteur = 0;
+const times = 100;
+
 // Connects to data-controller="game-config"
 export default class extends Controller {
-  static targets = ["compteur"];
+  static targets = ["compteurValue", "totalValue"];
 
   connect() {
-    console.log("game-config init Second try");
+    console.log("game-config init");
   }
 
   sleep(ms) {
@@ -28,7 +32,8 @@ export default class extends Controller {
     })
     .then(response => response.json())
     .then((data) => {
-      console.log("Session id :",data.session_id);
+      Compteur = 0;
+      OldProduct = 0;
       this.createParticipant(url, data);
     });
   }
@@ -49,7 +54,6 @@ export default class extends Controller {
     .then(response => response.json())
     .then((data) => {
       console.log("Session Started");
-      const times = 100;
       this.startBike(times);
       this.getProduction(url, participant, times);
     });
@@ -98,7 +102,7 @@ export default class extends Controller {
   async getProduction(url, participant, times) {
     let i = 0;
     do {
-      console.log("getProduction function");
+      console.log(`Boucles : ${i}`);
       fetch(`${url}/participate/findByBikeIdAndSessionId/${participant.bike_id}/${participant.session_id}`, {
         method: "GET",
         headers: {"Content-Type": "application/json"}
@@ -106,10 +110,10 @@ export default class extends Controller {
       .then(response => response.json())
       .then((data) => {
         console.log("Get participate product :",data.participate_production);
-        this.showProduction(data.participate_production);
+        this.showProduction(data.participate_production, times);
       });
       i += 1;
-      await this.sleep(500);
+      await this.sleep(180);
 
       if (i === (times - 1)) {
         this.endSession(participant);
@@ -118,9 +122,17 @@ export default class extends Controller {
     
   }
 
-  showProduction(product) {
-    console.log("showProduction function");
-    this.compteurTarget.innerText = product;
+  showProduction(product, times) {
+
+    if (product > OldProduct) {
+      Compteur = Number(product) - OldProduct;
+      OldProduct = Number(product);
+      console.log("changement de la valeurs");
+    } 
+    //On renvoie :
+    this.totalValueTarget.innerText = product;
+
+    this.compteurValueTarget.innerText = Compteur;
   }
 
   startBike(times) {
