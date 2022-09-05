b2a6011cd4633a454e241352417b9c2f1b9a7a65
jeanne desmier <desmierjeanne@gmail.com>
2022-09-05 16:59:40 +0200

    ajout points quizz

--- app/javascript/controllers/quizz_answer_controller.js
+++ app/javascript/controllers/quizz_answer_controller.js
@@ -1,11 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
-
+let sum;
 // Connects to data-controller="quizz-answer"
 export default class extends Controller {
   connect() {
-    console.log("controller quizz")
+     console.log("controller quizz")
   }
-  static targets = ["frigo", "lavelinge", "smartphone", "tv"]
+  static targets = ["frigo", "lavelinge", "smartphone", "tv", "score", "gsid" ]
+  static values = {
+    points: Number,
+  }
 
   frigo(event) {
     this.frigoTarget.classList.replace('quizz-choice-frigo', 'quizz-choice-frigo-active')
@@ -23,7 +26,35 @@ export default class extends Controller {
   }
 
   lavelinge(event) {
+    const goodAnswer = 10
     this.lavelingeTarget.classList.replace('quizz-choice-lavelinge', 'quizz-choice-lavelinge-active')
+    const pts = this.pointsValue
+    sum = pts + goodAnswer
+    console.log(sum)
+    this.scoreTarget.innerText = sum
+    console.log(this.gsidTarget.innerText)
+    this.updateScoreDB()
+  }
+
+  updateScoreDB(){
+
+    let id = this.gsidTarget.innerText
+    fetch(`/gamingsessions/${id}`, {
+      method: "PATCH",
+      headers: {'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        score: sum
+      })
+    })
+    .then(response => () => {
+      if (response.status === 200) {
+        console.log("Ok");
+      }
+    })
+    .then(() => {
+      console.log("save to db")
+    })
   }
 
 }
