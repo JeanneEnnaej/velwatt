d2c2169ee71039e9a6ca9e5bfd7d22d7753695eb
Alexis Barreirinho <alexis@barreirinho.com>
2022-09-07 19:09:59 +0200

    disconnect & QR code

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -6,13 +6,13 @@ class BikesController < ApplicationController
   def game
     ## créer nouvelle session et mettre l'id en interpolation pour QR code
     # Création du qrcode avec l'url du show de la session
-    @qr_code = RQRCode::QRCode.new("http://localhost:3000/gamingsessions/2")
+    # @qr_code = RQRCode::QRCode.new("http://localhost:3000/gamingsessions/2")
     # permet de mettre le qr code en forme carré noir et blanc
-    @svg = @qr_code.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 6
-    )
+    # @svg = @qr_code.as_svg(
+    #   offset: 0,
+    #   color: '000',
+    #   shape_rendering: 'crispEdges',
+    #   module_size: 6
+    # )
   end
 end
