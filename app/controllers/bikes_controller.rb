564282211e48bfac890235b117c63e16b7f34020
amlds.dev@gmail.com <amlds.dev@gmail.com>
2022-09-08 16:28:57 +0200

    debug controller game

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -4,16 +4,5 @@ class BikesController < ApplicationController
 
   # jeu
   def game
-    ## créer nouvelle session et mettre l'id en interpolation pour QR code
-    # Création du qrcode avec l'url du show de la session
-    # @qr_code = RQRCode::QRCode.new("http://localhost:3000/gamingsessions/2")
-    # permet de mettre le qr code en forme carré noir et blanc
-    @svg = @qr_code.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 6,
-      viewbox: true
-    )
   end
 end
