104efee5523637f9fecb6f21a99e2e5d7a2a2f28
jeanne desmier <desmierjeanne@gmail.com>
2022-09-09 11:39:51 +0200

    changement de wallet seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ p "Go la seed !"
 
 p "Seed users"
 
-User.create!(first_name: "Jeanne", last_name: "Desmier", email: "desmierjeanne@gmail.com", password: "lewagon", wallet: 100)
+User.create!(first_name: "Jeanne", last_name: "Desmier", email: "desmierjeanne@gmail.com", password: "lewagon", wallet: 150)
 
 p 'seed bike'
 
