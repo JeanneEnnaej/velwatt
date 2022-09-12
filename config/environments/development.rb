65bfe7410d1fea8d4b30264f03e91198f95a6334
amlds.dev@gmail.com <amlds.dev@gmail.com>
2022-09-12 09:04:51 +0200

    branche delete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,5 +69,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << /(\w|\W)+\.ngrok\.io/
+  config.hosts << /(\w|\W)+\.loca\.lt/
 end
